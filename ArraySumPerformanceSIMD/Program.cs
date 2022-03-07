5af5fc0e1753a8a7833c9c8ec6fe39df15891200
Steven Giesel <stgiesel35@gmail.com>
2022-03-07 20:41:40 +0100

    Update readmes

--- ArraySumPerformanceSIMD/Program.cs
+++ ArraySumPerformanceSIMD/Program.cs
@@ -6,46 +6,46 @@ BenchmarkRunner.Run<ArrayAdd>();
 
 public class ArrayAdd
 {
-    private static readonly int[] Numbers = Enumerable.Repeat(1, 512).ToArray();
+private static readonly int[] Numbers = Enumerable.Repeat(1, 512).ToArray();
 
-    [Benchmark(Baseline = true)]
-    public int GetSumNaive()
+[Benchmark(Baseline = true)]
+public int GetSumNaive()
+{
+    var result = 0;
+    for (var index = 0; index < Numbers.Length; index++)
     {
-        var result = 0;
-        for (var index = 0; index < Numbers.Length; index++)
-        {
-            var i = Numbers[index];
-            result += i;
-        }
-
-        return result;
+        var i = Numbers[index];
+        result += i;
     }
 
-    [Benchmark]
-    public int SumLINQ() => Numbers.Sum();
+    return result;
+}
 
-    [Benchmark]
-    public int SumPLINQ() => Numbers.AsParallel().Sum();
+[Benchmark]
+public int SumLINQ() => Numbers.Sum();
+
+[Benchmark]
+public int SumPLINQ() => Numbers.AsParallel().Sum();
 
 
-    [Benchmark]
-    public int SIMDVectors()
+[Benchmark]
+public int SIMDVectors()
+{
+    // This depends on your hardware
+    // It basically says on how many entries can we performance this single operation
+    // aka how big is "multiple"
+    var vectorSize = Vector<int>.Count;
+    var accVector = Vector<int>.Zero;
+
+    // We are tiling the original list by the hardware vector size
+    for (var i = 0; i <= Numbers.Length - vectorSize; i += vectorSize)
     {
-        // This depends on your hardware
-        // It basically says on how many entries can we performance this single operation
-        // aka how big is "multiple"
-        var vectorSize = Vector<int>.Count;
-        var accVector = Vector<int>.Zero;
-
-        // We are tiling the original list by the hardware vector size
-        for (var i = 0; i <= Numbers.Length - vectorSize; i += vectorSize)
-        {
-            var v = new Vector<int>(Numbers, i);
-            accVector = Vector.Add(accVector, v);
-        }
-
-        // Scalar-Product of our vector against the Unit vector is its sum
-        var result = Vector.Dot(accVector, Vector<int>.One);
-        return result;
+        var v = new Vector<int>(Numbers, i);
+        accVector = Vector.Add(accVector, v);
     }
+
+    // Scalar-Product of our vector against the Unit vector is its sum
+    var result = Vector.Dot(accVector, Vector<int>.One);
+    return result;
+}
 }
---
+++ ArraySumPerformanceSIMD/README.md
@@ -0,0 +1,5 @@
+﻿# Branch prediction - cost of an if
+
+The blog post shows how branch prediction impacts the performance of your if statements
+
+Found [here](https://steven-giesel.com/blogPost/0c84f371-08e3-4a57-bdd7-8b08cb308232)
