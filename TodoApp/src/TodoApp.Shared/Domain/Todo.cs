83f6c9cad2cbd6e1c92cd03d84743aa4e034d861
Steven Giesel <stgiesel35@gmail.com>
2022-03-12 19:35:09 +0100

    Added TitleBar

---
+++ TodoApp/src/TodoApp.Shared/Domain/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TodoApp.Domain
+{
+    public enum KanbanState { New, InProgress, Done }
+
+    public class Todo
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public KanbanState KanbanState { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public bool IsOverdue => (DueDate ?? DateTime.MaxValue) < DateTime.Now;
+    }
+}
---
+++ TodoApp/src/TodoApp.Shared/TitleBar.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace TodoApp
+{
+    public sealed partial class TitleBar : UserControl
+    {
+        public TitleBar()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
