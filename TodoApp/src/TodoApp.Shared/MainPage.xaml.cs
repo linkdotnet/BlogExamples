88153d6ce8daaaceb06091d67911ce45dbb24835
Steven Giesel <stgiesel35@gmail.com>
2022-03-25 20:18:19 +0100

    Added state load and save

--- TodoApp/src/TodoApp.Shared/MainPage.xaml.cs
+++ TodoApp/src/TodoApp.Shared/MainPage.xaml.cs
@@ -1,3 +1,8 @@
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using Windows.Storage;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace TodoApp
@@ -9,6 +14,30 @@ namespace TodoApp
             InitializeComponent();
             DataContext = new MainPageViewModel();
             addItemButton.TodoItemCreated += (o, item) => ((MainPageViewModel)DataContext).TodoItems.Add(item);
+            Loaded += RecoverState;
+            LostFocus += (s, e) => SaveState();
+        }
+
+        private async void SaveState()
+        {
+            var folder = ApplicationData.Current.LocalFolder;
+            var file = await folder.CreateFileAsync("todoapp.json", CreationCollisionOption.OpenIfExists);
+            await FileIO.WriteTextAsync(file, JsonConvert.SerializeObject(DataContext));
+        }
+
+        private async void RecoverState(object sender, RoutedEventArgs e)
+        {
+            var folder = ApplicationData.Current.LocalFolder;
+            var file = await folder.TryGetItemAsync("todoapp.json");
+            if (file != null)
+            {
+                var text = await FileIO.ReadTextAsync(file as IStorageFile);
+                var viewmodel = JsonConvert.DeserializeObject<MainPageViewModel>(text);
+                if (viewmodel?.TodoItems.Any() == true)
+                {
+                    ((MainPageViewModel)DataContext).TodoItems.AddRange(viewmodel.TodoItems);
+                }
+            }            
         }
     }
 }
