d414f13c6325f33ab6d84b0895239fc74b14f99b
Steven Giesel <stgiesel35@gmail.com>
2022-04-03 12:48:29 +0200

    Added ValueStringBuilder

---
+++ ValueStringBuilder/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using LinkDotNet;
+
+ BenchmarkRunner.Run<Benchi>();
+
+[MemoryDiagnoser]
+public class Benchi
+{
+    [Benchmark(Baseline = true)]
+    public string DotNet()
+    {
+        var reference = new StringBuilder();
+        return reference
+            .AppendLine("Hello World")
+            .AppendLine("Here some other text")
+            .AppendLine("And again some other text as well for good measure")
+            .AppendLine("You are still here?")
+            .AppendLine("Hmmm.")
+            .AppendLine("I wish you a very nice day and all the best.")
+            .AppendLine("Sincerly Steven")
+            .ToString();
+    }
+
+    [Benchmark]
+    public string Fast()
+    {
+        var fast = new ValueStringBuilder();
+        fast.AppendLine("Hello World");
+        fast.AppendLine("Here some other text");
+        fast.AppendLine("And again some other text as well for good measure");
+        fast.AppendLine("You are still here?");
+        fast.AppendLine("I wish you a very nice day and all the best.");
+        fast.AppendLine("Sincerly Steven");
+        return fast.ToString();
+    }
+}
---
+++ ValueStringBuilder/ValueStringBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace LinkDotNet;
+
+[SkipLocalsInit]
+public ref struct ValueStringBuilder
+{
+    private int _bufferPosition;
+    private Span<char> _buffer;
+
+    public ValueStringBuilder()
+    {
+        _bufferPosition = 0;
+        _buffer = new char[32];
+    }
+
+    public ref char this[int index] => ref _buffer[index];
+
+    public void Append(char c)
+    {
+        if (_bufferPosition == _buffer.Length - 1)
+        {
+            Grow();
+        }
+
+        _buffer[_bufferPosition++] = c;
+    }
+
+    public void Append(ReadOnlySpan<char> str)
+    {
+        var newSize = str.Length + _bufferPosition;
+        if (newSize > _buffer.Length)
+            Grow(newSize * 2);
+
+        str.CopyTo(_buffer[_bufferPosition..]);
+        _bufferPosition += str.Length;
+    }
+
+    public void AppendLine(ReadOnlySpan<char> str)
+    {
+        Append(str);
+        Append(Environment.NewLine);
+    }
+
+    public override string ToString() => new(_buffer[.._bufferPosition]);
+
+    private void Grow(int capacity = 0)
+    {
+        var currentSize = _buffer.Length;
+        var newSize = capacity > 0 ? capacity : currentSize * 2;
+        var rented = ArrayPool<char>.Shared.Rent(newSize);
+        _buffer.CopyTo(rented);
+        _buffer = rented;
+        ArrayPool<char>.Shared.Return(rented);
+    }
+}
