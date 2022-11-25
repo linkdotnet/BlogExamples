89764c0ce5240ad1ff7179c91139884f346fb718
Steven Giesel <stgiesel35@gmail.com>
2022-11-25 13:35:52 +0100

    Added FrozenSet Benchmarks

---
+++ FrozenSetBenchmark/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Frozen;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Benchi>();
+
+public class Benchi
+{
+    private const int OneHundred = 100;
+    private readonly List<int> oneToHundredList = Enumerable.Range(0, OneHundred).ToList();
+    private readonly FrozenSet<int> oneToHundredFrozen = Enumerable.Range(0, OneHundred).ToFrozenSet();
+    private readonly HashSet<int> oneToHundredHashSet = Enumerable.Range(0, OneHundred).ToHashSet();
+    private readonly HashSet<int> oneToHundredImmutableHashSet= Enumerable.Range(0, OneHundred).ToHashSet();
+
+    [Benchmark(Baseline = true)]
+    public void LookupList()
+    {
+        for (var i = 0; i < OneHundred; i++)
+            _ = oneToHundredList.Contains(i);
+    }
+
+    [Benchmark]
+    public void LookupFrozen()
+    {
+        for (var i = 0; i < OneHundred; i++)
+            _ = oneToHundredFrozen.Contains(i);
+    }
+
+    [Benchmark]
+    public void LookupHashSet()
+    {
+        for (var i = 0; i < OneHundred; i++)
+            _ = oneToHundredHashSet.Contains(i);
+    }
+
+    [Benchmark]
+    public void LookupImmutableHashSet()
+    {
+        for (var i = 0; i < OneHundred; i++)
+            _ = oneToHundredImmutableHashSet.Contains(i);
+    }
+}
---
+++ FrozenSetBenchmark/README.md
@@ -0,0 +1,7 @@
+# Frozen collections in .NET 8
+
+.NET 7 was freshly released but Microsoft does not sleep. .NET 8 is already in the making and I want to showcase to you one new area where the dotnet team is working on **Frozen collections**. 
+
+So let's have a look at what **frozen collections** are and how they are working.
+
+Found [here](https://steven-giesel.com/blogPost/34e0fd95-0b3f-40f2-ba2a-36d1d4eb5601)
