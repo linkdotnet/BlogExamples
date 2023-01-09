08e08759eaaf92337336184d68842f398a35d81c
Steven Giesel <stgiesel35@gmail.com>
2023-01-09 08:48:17 +0100

    Remove comment and added Async overload

--- PaginationEF/PagedListQueryableExtensions.cs
+++ PaginationEF/PagedListQueryableExtensions.cs
@@ -9,13 +9,10 @@ public static class PagedListQueryableExtensions
         var count = await source.CountAsync(token);
         if (count > 0)
         {
-            // Be careful when you have NVARCHAR(MAX) in
-            // combination with ToListAsync
-            // See here: https://stackoverflow.com/questions/28543293/entity-framework-async-operation-takes-ten-times-as-long-to-complete/28619983
             var items = source
                 .Skip((page - 1) * pageSize)
                 .Take(pageSize)
-                .ToList();
+                .ToListAsync(token);
             return new PagedList<T>(items, count, page, pageSize);
         }
 
