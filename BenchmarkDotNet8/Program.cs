25333b4679398f14ad8f47a2e241324bd7facd8e
Steven Giesel <stgiesel35@gmail.com>
2023-04-13 13:18:34 +0200

    Update SB

--- BenchmarkDotNet8/Program.cs
+++ BenchmarkDotNet8/Program.cs
@@ -13,29 +13,30 @@ BenchmarkRunner.Run<Benchmarks>();
 public class Benchmarks
 {
     private readonly List<int> _numbers = Enumerable.Range(0, 10000).Select(s => Random.Shared.Next()).ToList();
-    private readonly string _text1 = "Hello woRld! ThIs is a test.It CoUlD be a lot longer, but it'S not. Or is it?";
-    private readonly string _text2 = "Hello world! This is a test.it could be a lot longer, but it's not. Or is it?";
+    private const string Text1 = "Hello woRld! ThIs is a test.It CoUlD be a lot longer, but it'S not. Or is it?";
+
+    private const string Text2 = "Hello world! This is a test.it could be a lot longer, but it's not. Or is it?";
 
     [Benchmark]
-    public string ReplaceStringBuilder() => new StringBuilder(_text1).Replace("Hello", "Goodbye").ToString();
+    public string ReplaceStringBuilder() => new StringBuilder(Text1).Replace("Hello", "Goodbye").ToString();
 
     [Benchmark]
-    public bool EqualsOrdinalIgnoreCase() => _text1.Equals(_text2, StringComparison.OrdinalIgnoreCase);
+    public bool EqualsOrdinalIgnoreCase() => Text1.Equals(Text2, StringComparison.OrdinalIgnoreCase);
 
     [Benchmark]
-    public bool EqualsOrdinal() => _text1.Equals(_text2, StringComparison.Ordinal);
+    public bool EqualsOrdinal() => Text1.Equals(Text2, StringComparison.Ordinal);
 
     [Benchmark]
-    public bool ContainsOrdinalIgnoreCase() => _text1.Contains("Or is it?", StringComparison.OrdinalIgnoreCase);
+    public bool ContainsOrdinalIgnoreCase() => Text1.Contains("Or is it?", StringComparison.OrdinalIgnoreCase);
 
     [Benchmark]
-    public bool ContainsOrdinal() => _text1.Contains("Or is it?", StringComparison.Ordinal);
+    public bool ContainsOrdinal() => Text1.Contains("Or is it?", StringComparison.Ordinal);
 
     [Benchmark]
-    public bool StartsWithOrdinalIgnoreCase() => _text1.StartsWith("Hello World", StringComparison.OrdinalIgnoreCase);
+    public bool StartsWithOrdinalIgnoreCase() => Text1.StartsWith("Hello World", StringComparison.OrdinalIgnoreCase);
 
     [Benchmark]
-    public bool StartsWithOrdinal() => _text1.StartsWith("Hello World", StringComparison.Ordinal);
+    public bool StartsWithOrdinal() => Text1.StartsWith("Hello World", StringComparison.Ordinal);
 
     [Benchmark]
     public int LinqMin() => _numbers.Min();
@@ -50,7 +51,7 @@ public class Benchmarks
     public string InvokeMethodViaReflection()
     {
         var method = typeof(string).GetMethod("ToLowerInvariant", BindingFlags.Public | BindingFlags.Instance);
-        return (string)method.Invoke(_text1, null);
+        return (string)method.Invoke(Text1, null);
     }
 
     [Benchmark]
