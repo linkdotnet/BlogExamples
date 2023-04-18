0756b5df00b16f9b6e834452379c1773181f4714
Steven Giesel <stgiesel35@gmail.com>
2023-04-18 17:56:55 +0200

    Update benchmarls

--- BenchmarkDotNet8/Program.cs
+++ BenchmarkDotNet8/Program.cs
@@ -14,6 +14,9 @@ BenchmarkRunner.Run<Benchmarks>();
 public class Benchmarks
 {
     private readonly List<int> _numbers = Enumerable.Range(0, 10000).Select(s => Random.Shared.Next()).ToList();
+    private readonly HashSet<int> _hashSet = Enumerable.Range(0, 10000).ToHashSet();
+    private readonly Dictionary<int, int> _dictionary = Enumerable.Range(0, 10000).ToDictionary(k => k, v => v);
+
     private const string Text1 = "Hello woRld! ThIs is a test.It CoUlD be a lot longer, but it'S not. Or is it?";
 
     private const string Text2 = "Hello world! This is a test.it could be a lot longer, but it's not. Or is it?";
@@ -65,10 +68,10 @@ public class Benchmarks
     public string[] EnumGetNames() => Enum.GetNames<DayOfWeek>();
 
     [Benchmark]
-    public List<int> ListAdd1000()
+    public List<int> ListAdd10000()
     {
         var list = new List<int>();
-        for (var i = 0; i < 1000; i++)
+        for (var i = 0; i < 10_000; i++)
         {
             list.Add(i);
         }
@@ -77,12 +80,38 @@ public class Benchmarks
     }
 
     [Benchmark]
-    public int ListLookup()
+    public int HashSetLookup()
     {
         var entriesFound = 0;
-        for (var i = 0; i < 100; i++)
+        for (var i = 0; i < 10000; i++)
         {
-            if (_numbers.Contains(i))
+            if (_hashSet.Contains(i))
+                entriesFound++;
+        }
+
+        return entriesFound;
+    }
+
+    [Benchmark]
+    public int DictionaryKeyLookup()
+    {
+        var entriesFound = 0;
+        for (var i = 0; i < 10000; i++)
+        {
+            if (_dictionary.ContainsKey(i))
+                entriesFound++;
+        }
+
+        return entriesFound;
+    }
+
+    [Benchmark]
+    public int DictionaryValueLookup()
+    {
+        var entriesFound = 0;
+        for (var i = 0; i < 10000; i++)
+        {
+            if (_dictionary.ContainsValue(i))
                 entriesFound++;
         }
 
